c89fc18900b2c5fa637e03888b886344c6ac95ed
BwunLevain <vivienne114bengtsson@gmail.com>
2026-04-02 15:24:35 +0200

    database models and file structure for Category and TimeLog

---
+++ OrderAPI/Features/TimeLog/TimeLog.cs
@@ -0,0 +1,15 @@
+﻿namespace OrderAPI.Features.TimeLog
+{
+    public class TimeLog
+    {
+        public int Id { get; set; }
+
+        public DateTime StartTime { get; set; } = DateTime.Now;
+
+        public DateTime? EndTime { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public virtual Category.Category? Category { get; set; }
+    }
+}
