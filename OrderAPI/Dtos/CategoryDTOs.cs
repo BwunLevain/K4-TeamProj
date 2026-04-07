6b03be40ad84dc7300c151d92faf0fb2cbf9d8cd
Johan <johan.soderdahl@chasacademy.se>
2026-04-07 11:56:54 +0200

    Added Dtos for TimeLog

---
+++ OrderAPI/Dtos/CategoryDTOs.cs
@@ -0,0 +1,21 @@
+﻿namespace OrderAPI.Dtos
+{
+    public record TimeLogResponse(
+        int Id,
+        DateTime StartTime,
+        DateTime? EndTime,
+        int CategoryId    
+    );
+
+    public record CreateTimeLogRequest(
+        DateTime StartTime,
+        DateTime? EndTime,
+        int CategoryId
+    );
+
+    public record UpdateTimeLogRequest(
+        DateTime? StartTime,
+        DateTime? EndTime,
+        int? CategoryId
+        );
+}
