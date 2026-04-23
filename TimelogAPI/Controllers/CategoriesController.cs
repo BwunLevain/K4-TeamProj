7c5cecd81607fdcd4234025fcf6c299a9d39eb02
Hande Bengü <hande.bengu@chasacademy.se>
2026-04-23 21:46:15 +0200

    fix:add controller contents

--- TimelogAPI/Controllers/CategoriesController.cs
+++ TimelogAPI/Controllers/CategoriesController.cs
@@ -1,6 +1,53 @@
-﻿namespace TimelogAPI.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using TimelogAPI.Features.Categories.Dtos;
+using TimelogAPI.Services;
+
+namespace TimelogAPI.Controllers
 {
-    public class CategoriesController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
     {
+        private readonly ICategoryService _categoryService;
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory([FromBody] CreateCategoryRequest request)
+        {
+            var result = await _categoryService.CreateCategoryAsync(request);
+            return CreatedAtAction(nameof(GetCategoryById), new { id = result.Id }, result);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetCategoryById(int id)
+        {
+            var result = await _categoryService.GetCategoryByIdAsync(id);
+            return Ok(result);              
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetCategories([FromQuery] int page = 1,
+        [FromQuery] int pageSize = 20,
+        [FromQuery] string? searchTerm = null)
+        {
+            var result = await _categoryService.GetPagedCategoriesAsync(page, pageSize, searchTerm);
+            return Ok(result);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateCategory(int id, [FromBody] UpdateCategoryRequest request)
+        {
+            var result = await _categoryService.UpdateCategoryAsync(id, request);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCategory(int id)
+        {
+            var result = await _categoryService.DeleteCategoryAsync(id);
+            return NoContent();
+        }
     }
-}
--- TimelogAPI/Controllers/TimelogsController.cs
+++ TimelogAPI/Controllers/TimelogsController.cs
@@ -1,6 +1,55 @@
-﻿namespace TimelogAPI.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using TimelogAPI.Features.TimeLogs.Dtos;
+using TimelogAPI.Services;
+
+namespace TimelogAPI.Controllers
 {
-    public class TimelogsController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TimelogsController : ControllerBase
     {
+        private readonly ITimelogService _timelogService;
+        public TimelogsController(ITimelogService timelogService)
+        {
+            _timelogService = timelogService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateTimelog([FromBody] CreateTimeLogRequest request)
+        {
+            var result = await _timelogService.CreateTimelogAsync(request);
+            return CreatedAtAction(nameof(GetTimelogById), new { id = result.Id }, result);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetTimelogById(int id)
+        {
+            var result = await _timelogService.GetTimelogByIdAsync(id);
+            return Ok(result);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetTimelogs([FromQuery] int page = 1,
+        [FromQuery] int pageSize = 20,
+        [FromQuery] string? category = null,
+        [FromQuery] string? sortBy = null)
+        {
+            var result = await _timelogService.GetTimelogsAsync(category, sortBy, page, pageSize);
+            return Ok(result);
+        }
+
+        [HttpPut("{id}")]
+
+        public async Task<IActionResult> UpdateTimelog(int id,[FromBody] UpdateTimeLogRequest request)
+        {
+            var result = await _timelogService.UpdateTimelogAsync(id, request);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTimelog(int id)
+        {
+            var result = await _timelogService.DeleteTimelogAsync(id);
+            return NoContent();
+        }
     }
-}
