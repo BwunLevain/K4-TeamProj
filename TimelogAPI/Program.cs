14cf6342c5244a5e7f5abc50733c8afaf82853df
BwunLevain <vivienne114bengtsson@gmail.com>
2026-04-27 13:37:44 +0200

    Add using directive for TimelogAPI.Middleware in Program.cs

--- TimelogAPI/Program.cs
+++ TimelogAPI/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
 using TimelogAPI.Services;
+using TimelogAPI.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
